b37e2d24e288da17cb2f342570669d26f16bffbe
Maria Niku <maria.niku@cs.helsinki.fi>
2016-08-26 09:50:41 +0300

    added date to recent items display

--- application/views/scripts/items/single.php
+++ application/views/scripts/items/single.php
@@ -2,16 +2,20 @@
     <?php
     $title = metadata($item, array('Dublin Core', 'Title'));
     $description = metadata($item, array('Dublin Core', 'Description'), array('snippet' => 150));
+    $date = metadata($item, array('Dublin Core', 'Date'));
     ?>
     <h3><?php echo link_to($item, 'show', strip_formatting($title)); ?></h3>
     <?php if (metadata($item, 'has files')) {
         echo link_to_item(
-            item_image('square_thumbnail', array(), 0, $item), 
+            item_image('square_thumbnail', array(), 0, $item),
             array('class' => 'image'), 'show', $item
         );
     }
     ?>
     <?php if ($description): ?>
-        <p class="item-description"><?php echo $description; ?></p>
+        <p class="item-description">
+          <?php echo 'Kirjoitusaika: '.date('j.n.Y', strtotime($date)); ?><br />
+          <?php echo $description; ?>
+        </p>
     <?php endif; ?>
 </div>
